55b1571bed53be654309110e1fbc0ab55dfea646
sangwon <cold929@naver.com>
2022-01-14 14:29:56 +0900

    CT

---
+++ seohyun/BOJ/test.cpp
@@ -0,0 +1,10 @@
+//BOJ [문제이름(문제번호)]
+#include <iostream>
+using namespace std;
+
+int main(void){
+	
+hello
+
+    return 0;
+}
---
+++ seohyun/Programmers/test.cpp
@@ -0,0 +1,10 @@
+//프로그래머스 [문제이름]
+#include <iostream>
+using namespace std;
+
+int main(void){
+	
+hello
+
+    return 0;
+}
